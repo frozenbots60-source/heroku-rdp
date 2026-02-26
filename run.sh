f439e306e7d4fdc641acb7348b900986f343851b
frozenbots60-source <frozenbots60@gmail.com>
2026-02-26 23:25:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Ensure the DISPLAY variable is set for any app we run
 export DISPLAY=:0
-
-# Start Supervisor (which starts Xvfb, VNC, and the Web UI)
-# It uses the config file we created above
 /usr/bin/supervisord -c /app/supervisord.conf

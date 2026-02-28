e83181d7366bf53ee718827dadb1702800c54aad
frozenbots60-source <frozenbots60@gmail.com>
2026-02-28 10:40:33 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+# 1. Create the directory where the extension will live
+mkdir -p /tmp/my-extension
+
+# 2. Download the extension zip file from the provided URL
+wget -qO /tmp/extension.zip https://kustbots.github.io/file/claimer-v3.1.zip
+
+# 3. Extract the contents of the zip file into the target folder
+unzip -q -o /tmp/extension.zip -d /tmp/my-extension
+
+# 4. Clean up the downloaded zip file to keep the container clean
+rm /tmp/extension.zip
+
+# 5. Start the original services
 export DISPLAY=:0
 /usr/bin/supervisord -c /app/supervisord.conf

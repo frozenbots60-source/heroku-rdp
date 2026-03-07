e5d233357f8d74c271aa7b17dd303f383a280b6b
frozenbots60-source <frozenbots60@gmail.com>
2026-03-07 12:18:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# 1. Create the directory where the extension will live
-mkdir -p /tmp/my-extension
+# The extension code is already in the repo/container at this point.
+# If you need to move it to a specific profile location before launch, 
+# you can add that logic here, otherwise, we head straight to services.
 
-# 2. Download the extension zip file from the provided URL
-wget -qO /tmp/extension.zip https://filehosting.kustbotsweb.workers.dev/f/d1ba7f3d5de649a79b7e6964b5cc5f84
-
-# 3. Extract the contents of the zip file into the target folder
-unzip -q -o /tmp/extension.zip -d /tmp/my-extension
-
-# 4. Clean up the downloaded zip file to keep the container clean
-rm /tmp/extension.zip
-
-# 5. Start the original services
+# 1. Start the services
 export DISPLAY=:0
 /usr/bin/supervisord -c /app/supervisord.conf
